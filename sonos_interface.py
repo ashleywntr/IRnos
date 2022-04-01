5c2782010798d479bac131edf42a9eae10ead306
Ashley Winter <ashleywntr@gmail.com>
2022-04-01 22:09:04 +0100

    sonos control functionality

--- sonos_interface.py
+++ sonos_interface.py
@@ -1,1 +1,26 @@
-from soco import di
+from soco import discover
+from enum import Enum
+
+
+class Volume(Enum):
+    UP = 1
+    DOWN = -1
+    MUTE = 0
+
+
+def select_speaker(speaker_name):
+    for speaker in discover():
+        if speaker.player_name == speaker_name:
+            return speaker
+
+
+def volume_logic(volume_change_direction, speaker):
+    working_speaker = select_speaker(speaker)
+
+    if volume_change_direction == Volume.UP:
+        working_speaker.volume += 3
+    elif volume_change_direction == Volume.DOWN:
+        working_speaker.volume -= 3
+    elif volume_change_direction == Volume.MUTE:
+        working_speaker.mute = not working_speaker.mute
+
